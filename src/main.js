297b86551cebe81d4bd400f716156428d7700b1d
webDevUs3r <webDevUser@mail.ru>
2024-10-18 19:49:52 +0300

    Рефакторинг презентера

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import TripInfoView from './view/trip-info-view.js';
 import TripPresenter from './presenter/trip-presenter.js';
 import {render, RenderPosition} from './render.js';
 
-const siteContentContainer = document.querySelector('.page-main .page-body__container');
+const siteContentContainer = document.querySelector('.trip-events');
 const siteHeaderContainer = document.querySelector('.page-header');
 const filtersContainer = siteHeaderContainer.querySelector('.trip-controls__filters');
 const headerContentContainer = siteHeaderContainer.querySelector('.trip-main');
--- src/presenter/trip-presenter.js
+++ src/presenter/trip-presenter.js
@@ -3,26 +3,24 @@ import EditPointView from '../view/edit-point-view';
 import EventListView from '../view/event-list-view';
 import PointView from '../view/point-view';
 import SortView from '../view/sort-view';
-import TripView from '../view/trip-view';
 
 const POINT_COUNT = 3;
 
 export default class TripPresenter {
-  tripComponent = new TripView();
-  eventListContainer = new EventListView();
+  sortComponent = new SortView();
+  eventListComponent = new EventListView();
 
   constructor({tripContainer}) {
     this.tripContainer = tripContainer;
   }
 
   init() {
-    render(this.tripComponent, this.tripContainer);
-    render(new SortView(), this.tripComponent.getElement());
-    render(this.eventListContainer, this.tripComponent.getElement());
-    render(new EditPointView(), this.eventListContainer.getElement());
+    render(this.sortComponent, this.tripContainer);
+    render(this.eventListComponent, this.tripContainer);
+    render(new EditPointView(), this.eventListComponent.getElement());
 
     for (let i = 0; i < POINT_COUNT; i++) {
-      render(new PointView(), this.eventListContainer.getElement());
+      render(new PointView(), this.eventListComponent.getElement());
     }
   }
 }
